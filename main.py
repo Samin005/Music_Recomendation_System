958ed2c5232388f2fc2e1373be72cc1bde29d8b9
Samin Azhan <samin005.bracu@gmail.com>
2020-05-31 17:55:54 +0600

    Improved user experience by running in a loop until the user wants to exit program.

--- main.py
+++ main.py
@@ -15,13 +15,24 @@ label = music_data_frame.iloc[:, -1]
 decision_tree_model = DecisionTreeClassifier()
 decision_tree_model.fit(features, label)
 
-input_set_1 = [21, 1]
-input_set_2 = [22, 0]
+# Test with sample inputs
 
-print(decision_tree_model.predict([input_set_1, input_set_2]))
+# input_set_1 = [21, 1]
+# input_set_2 = [22, 0]
+#
+# print(decision_tree_model.predict([input_set_1, input_set_2]))
 
 # take input from the user
-user_age = input('Please enter age: ')
-user_gender = input('Are you a male(1) or female(0)?')
-user_input_set = [user_age, user_gender]
-print(decision_tree_model.predict([user_input_set]))
+play_again = 'yes'
+while 'y' in play_again:
+    user_age = input('Please enter age: ')
+    user_gender = input('Are you a male or female? ')
+    if 'f' in user_gender.lower():
+        user_gender = 0
+    else:
+        user_gender = 1
+    user_input_set = [user_age, user_gender]
+    print(user_input_set)
+    print(f"You probably like {decision_tree_model.predict([user_input_set])[0]} music!")
+    play_again = input('Would you like to play again? ').lower()
+print('Music Recommendation Program Terminated.')
